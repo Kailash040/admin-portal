53ccf44dabad331ff33402744496aef4b4e42abe
kailash <kailashrajut0575@gmail.com>
2023-09-21 18:19:14 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div >
       
-     {/* <h1 className='text-center'>Admin-Portal</h1> */}
      <Router>
       <Routes>
         <Route path="/" element={<Home />} />
